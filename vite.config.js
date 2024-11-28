af4703031d6a154152868ede0975e4707d56332d
Muhammed Bilal Omeiza <104626610+Bilcruz1@users.noreply.github.com>
2024-11-28 12:51:50 +0100

    fixes

--- vite.config.js
+++ vite.config.js
@@ -9,12 +9,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-	plugins: [react()],
+// export default defineConfig({
+// 	plugins: [react()],
+// 	server: {
+// 		host: '0.0.0.0', // Allow connections from any device
+// 		port: 5173, // Use the same port
+// 		strictPort: true, // Ensure the port doesn’t change
+// 		open: true, // Automatically open in browser on desktop (optional)
+// 	},
+// });
+export default {
 	server: {
-		host: '0.0.0.0', // Allow connections from any device
-		port: 5173, // Use the same port
-		strictPort: true, // Ensure the port doesn’t change
-		open: true, // Automatically open in browser on desktop (optional)
+		port: 5173, // Default port
+		strictPort: false, // Allows Vite to use the next available port if 5173 is taken
 	},
-});
+};
