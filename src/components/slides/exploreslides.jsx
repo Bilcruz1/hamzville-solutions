96c908a948d3fe68ef387354c5cdc6d33ffbbadb
Muhammed Bilal Omeiza <104626610+Bilcruz1@users.noreply.github.com>
2024-11-28 19:06:17 +0100

    exploreimages

--- src/components/slides/exploreslides.jsx
+++ src/components/slides/exploreslides.jsx
@@ -6,28 +6,45 @@ import 'swiper/css/navigation'; // Navigation module styles
 
 // Your images here
 import img1 from '../../assets/slides/exploreslides/slide.jpg';
-import img2 from '../../assets/slides/exploreslides/slide1.jpg';
-import img3 from '../../assets/slides/exploreslides/slide2.jpg';
-import img4 from '../../assets/slides/exploreslides/slide3.jpg';
-import img5 from '../../assets/slides/exploreslides/slide4.jpg';
-import img6 from '../../assets/slides/exploreslides/slide5.jpg';
-import img7 from '../../assets/slides/exploreslides/slide6.jpg';
-import img8 from '../../assets/slides/exploreslides/slide7.jpg';
-import img9 from '../../assets/slides/exploreslides/slide8.jpg';
-import img10 from '../../assets/slides/exploreslides/slide9.jpg';
-import img11 from '../../assets/slides/exploreslides/slide10.jpg';
-import img12 from '../../assets/slides/exploreslides/slide11.jpg';
-import img13 from '../../assets/slides/exploreslides/slide12.jpg';
-import img14 from '../../assets/slides/exploreslides/slide13.jpg';
-import img15 from '../../assets/slides/exploreslides/slide14.jpg';
-import img16 from '../../assets/slides/exploreslides/slide15.jpg';
+import img2 from '../../assets/slides/exploreslides/Slide1.jpg';
+import img3 from '../../assets/slides/exploreslides/Slide2.jpg';
+import img4 from '../../assets/slides/exploreslides/Slide3.jpg';
+import img5 from '../../assets/slides/exploreslides/Slide4.jpg';
+import img6 from '../../assets/slides/exploreslides/Slide5.jpg';
+import img7 from '../../assets/slides/exploreslides/Slide6.jpg';
+import img8 from '../../assets/slides/exploreslides/Slide7.jpg';
+import img9 from '../../assets/slides/exploreslides/Slide8.jpg';
+import img10 from '../../assets/slides/exploreslides/Slide9.jpg';
+import img11 from '../../assets/slides/exploreslides/Slide10.jpg';
+import img12 from '../../assets/slides/exploreslides/Slide11.jpg';
+import img13 from '../../assets/slides/exploreslides/Slide12.jpg';
+import img14 from '../../assets/slides/exploreslides/Slide13.jpg';
+import img15 from '../../assets/slides/exploreslides/Slide14.jpg';
+import img16 from '../../assets/slides/exploreslides/Slide15.jpg';
 
 import arrowNext from '../../assets/icons/arrowNext.svg';
 import arrowBack from '../../assets/icons/arrowBack.svg';
 
 export default function ExploreSlides() {
 	const [currentSlide, setCurrentSlide] = useState(1); // State to track the current slide index
-	const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16];
+	const images = [
+		img1,
+		img2,
+		img3,
+		img4,
+		img5,
+		img6,
+		img7,
+		img8,
+		img9,
+		img10,
+		img11,
+		img12,
+		img13,
+		img14,
+		img15,
+		img16,
+	];
 
 	const handleSlideChange = swiper => {
 		setCurrentSlide(swiper.realIndex + 1); // Update the current slide number (0-indexed so we add 1)
